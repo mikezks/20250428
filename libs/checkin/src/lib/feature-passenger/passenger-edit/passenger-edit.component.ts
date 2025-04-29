1b68d5df4407753b416a66aa5231c2055f7dec71
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-04-29 13:31:40 +0200

    refactor: passenger load rxjs interop

--- libs/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,7 +1,11 @@
 import { NgIf } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, input, numberAttribute, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { validatePassengerStatus } from '../../util-validation';
+import { initialPassenger } from '../../logic-passenger';
+import { PassengerService } from '../../logic-passenger/data-access/passenger.service';
+import { switchMap } from 'rxjs';
 
 
 @Component({
@@ -13,15 +17,30 @@ import { validatePassengerStatus } from '../../util-validation';
   templateUrl: './passenger-edit.component.html'
 })
 export class PassengerEditComponent {
+  private passengerService = inject(PassengerService);
+
+  id = input(0, { transform: numberAttribute });
+  id$ = toObservable(this.id);
+  passenger$ = this.id$.pipe(
+    switchMap(id => this.passengerService.findById(id))
+  );
+  passenger = toSignal(this.passenger$, {
+    initialValue: initialPassenger
+  });
+
   protected editForm = inject(NonNullableFormBuilder).group({
     id: [0],
-    firstName: [''],
+    firstName: ['INIT'],
     name: [''],
     bonusMiles: [0],
     passengerStatus: ['', [
       validatePassengerStatus(['A', 'B', 'C'])
     ]]
   });
+
+  constructor() {
+    effect(() => this.editForm.patchValue(this.passenger()));
+  }
 
   protected save(): void {
     console.log(this.editForm.value);
