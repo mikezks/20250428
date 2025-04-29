5138e69f33ea4923d52e7dbccde6b56d77a44be0
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-04-29 13:20:36 +0200

    refactor: hydrate rx forms from input w/ router cmp input binding

--- apps/shell/src/app/app.config.ts
+++ apps/shell/src/app/app.config.ts
@@ -1,15 +1,17 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouterFeature } from '@flight-demo/shared/state';
 import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { APP_ROUTES } from './app.routes';
-import { provideRouterFeature } from '@flight-demo/shared/state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(APP_ROUTES),
+    provideRouter(APP_ROUTES,
+      withComponentInputBinding()
+    ),
     provideHttpClient(),
     provideStore(),
     provideEffects(),
